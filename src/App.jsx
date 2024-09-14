5015b9782672a38d42f5a7917c71fe4cb0de34a7
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-09-14 21:07:32 +0200

    install depa

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <div>
-    test
+    test v-2.o
    </div>
   )
 }
