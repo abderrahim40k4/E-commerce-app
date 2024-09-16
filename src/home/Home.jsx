027072942d82bfd36a64f9751b6731dba35cc06f
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-09-16 16:26:50 +0200

    prodact header section

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import Category from './Category'
+import CategoryShowCase from './CategoryShowCase'
 
 const Home = () => {
   return (
     <div>
         <Banner/>
         <Category/>
+        <CategoryShowCase/>
     </div>
   )
 }
